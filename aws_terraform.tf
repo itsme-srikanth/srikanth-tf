92a57487ef0e55f4d426b26026e09f1556917a4d
itsme-srikanth <srikanthkurra111@gmail.com>
2023-09-15 13:38:20 +0530

    ecomm_public_subnet

--- aws_terraform.tf
+++ aws_terraform.tf
@@ -3,15 +3,16 @@ resource "aws_vpc" "Ecomm" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   tags = {
-     Name = "ecomm_vpc"                       
+     Name = "ecomm-vpc"                       
   }                          
 }
-resource "aws_subnet" "ecomm_pub_sn" {
-  vpc_id = aws_vpc.ecomm.id
-  cidr_block = 10.0.1.0/24
-  availability_zone = "ap-south-1a"
-  map_public_ip_enable = "true"
+# public subnet
+resource "aws_subnet" "ecomm-pub-sn" {
+  vpc_id         = aws_vpc.ecomm.id
+  cidr_block     = "10.0.1.0/24"
+  availability_zone   = "ap-south-1a"
+
   tags = {
-    Name = "ecomm_public_subnet"
+    Name = "ecomm-public-subnet"
   }
 }
